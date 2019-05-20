76e85e85945cbb0265b1fb652f4d287da6bf13d2
Benjamin Santelices <benjasantelices@hotmail.com>
2019-05-19 21:25:27 -0400

    -Added support for recieving and processing APRS-IS packets. -You can now poll rotor position in real time.

--- rotor/bearingElevCalc.py
+++ rotor/bearingElevCalc.py
@@ -11,43 +11,55 @@ class PolarCoordinate:  #object for storing and operating locations in 3D polar 
         self.altitude = alt  #define latitude, longitude and altitude variables.
 
     def distanceTo(self, point): #uses the haversine function for calculating the arc distance between two points.
-        a = math.pow(math.sin((self.latitude - point.latitude)/2), 2) + math.cos(self.longitude) * math.cos(point.longitude) * math.pow(math.sin((self.longitude - point.lognitude)/2), 2)
+        deltaLatHalf = math.radians((math.degrees(point.latitude) - math.degrees(self.latitude)) / 2)
+        deltaLonHalf = math.radians((math.degrees(point.longitude) - math.degrees(self.longitude)) / 2)
+
+        a = math.pow(math.sin(deltaLatHalf), 2) + (math.cos(self.latitude) * math.cos(point.latitude) * math.pow(math.sin(deltaLonHalf), 2))
         b = 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a))
-        distance = earthRadius * b
+        distance = self.earthRadius * b
         return distance
 
     def azimuthTo(self, point):  #calculates heading to a specific target. Also uses haversine function.
-        azimuth = math.atan2(math.sin(point.longitude - self.longitude) * math.cos(point.latitude), math.sin(point.latitude) - math.sin(self.latitude) * math.cos(point.latitude * math) * math.cos(point.longitude - self.longitude))
-        return math.degrees(azimuth)
+        deltaLon = math.radians(math.degrees(point.longitude) - math.degrees(self.longitude))
 
-    def commHopDistance(self, point): #calculates distance in a straight line between two
-        arcAngle = distanceTo(self, point) / earthRadius
-        commHopDist = math.sqrt(math.pow(self.altitude + earthRadius, 2) + math.pow(point.altitude + earthRadius, 2) - 2 * (self.altitude + earthRadius) * (point.altitude + earthRadius) * math.cos(arcAngle))
+        azi = math.atan2(math.sin(deltaLon) * math.cos(point.latitude), math.cos(self.latitude) * math.sin(point.latitude) - math.sin(self.latitude) * math.cos(point.latitude) * math.cos(deltaLon))
+        return math.degrees(azi)
+
+    def commHopDistance(self, point): #calculates distance in a straight line between two points.
+        arcAngle = self.distanceTo(point) / self.earthRadius
+        commHopDist = math.sqrt(math.pow(self.altitude + self.earthRadius, 2) + math.pow(point.altitude + self.earthRadius, 2) - 2 * (self.altitude + self.earthRadius) * (point.altitude + self.earthRadius) * math.cos(arcAngle))
         return commHopDist
 
-    def elevationTo(self, point):
-        arcAngle = distanceTo(self, point) / earthRadius
-        c = commHopDistance(self, point)
-        desiredVertex = asin((point.altitude + earthRadius) * math.sin(arcAngle) / c)
-        return math.degrees(desiredVertex) - 90
+    def elevationTo(self, point): #returns elevation between two points. NOT YET WORKING.
+        arcAngle = self.distanceTo(point) / self.earthRadius
+        c = self.commHopDistance(point)
+        desiredVertex = math.asin(((point.altitude + self.earthRadius) * math.sin(arcAngle)) / c)
+        return -(math.degrees(desiredVertex)  - 90)
 
-class EasyComm:
+class EasyComm: #manage connection between an EasyComm I antenna rotor, connected to a serial port.
 
     def __init__(self, serialPort):
-        self.UART = serial.Serial(port=serialPort)
+        self.UART = serial.Serial(port=serialPort) #pyserial serial port object.
+        self.measuredAzimuth
+        self.measuredElevation #azimuth and elevation obtained from getAngles()
 
-    def send(azimuth, elevation):
+    def send(self, azimuth, elevation):
         azString = '%05.1f' % azimuth
         elString = '%05.1f' % elevation
-        command = "AZ=" + azString + " EL=" + elString + "\n"
-        self.UART.write(command)
+        command = "AZ=" + azString + " EL=" + elString + "\n" #creates proper string in accordance to EasyComm I protocol
+        self.UART.write(command) #sends to rotor.
 
-    def getAngles():
+    def getAngles(self): #gets current measured angles for the rotor.
         self.UART.write("AZ EL\n")
+        serialIn = self.UART.read(self.UART.in_waiting()) #check buffer and get answer from rotor.
+        az, el = serialIn.split(" ")
+        self.measuredAzimuth = float(az[3:])
+        self.measuredElevation = float(el[3:]) #parse data and return measured data.
 
-class APRS:
+class APRS: #gets data from APRS-IS servers and returns important data.
+
     def __init__(self, callsign, target):
-        self.server = aprslib.IS(callsign)
+        self.server = aprslib.IS(callsign) #aprslib internet services connection
         self.target = target
 
     def callback(self, text):
@@ -59,9 +71,8 @@ class APRS:
         self.server.connect()
         self.server.consumer(callback)
 
-station = PolarCoordinate(0, 0, 0)
-sonde = PolarCoordinate(0, 0, 0)
-tracker = EasyComm('COM2')
-aprsis = APRS("CE3BUC")
 print "SPEL Radiosonde Tracker Station Software DEV v0.1"
-print "Initializing APRS Service"
+call = input("Your SSID: ")
+tget = input("Target SSID: ")
+print "Initializing APRS Service..."
+aprsis = APRS(call, tget)
